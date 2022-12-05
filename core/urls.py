fd66e67e8a565e7d764c6fa6b55c341fc8073bee
mastercoder9363 <118736675+mastercoder9363@users.noreply.github.com>
2022-12-05 15:05:10 +0300

    gul

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('gull/', gull)
+]
---
+++ core/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from frontend import *
+
+def gull(request):
+    return render(request, 'gul.html')
